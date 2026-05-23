66e3b3f8dc2ab442569eccc147810a10ea42bbbc
himanshu-coders <avasthihimanshu98@gmail.com>
2026-05-23 18:29:44 +0000

    third commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const account_Id=1444
+let accountEmail="avasthihimanshu98@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+ accountEmail="avasthi98@gmail.com"
+ accountPassword="6543"
+ accountCity="Balpur"
+// account_Id=2 not allowed
+console.table([accountCity,accountEmail,accountPassword,accountCity])
+console.log(account_Id);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// treat all JS code as newer version
+// alert("Hello World") we are using nodejs, not browser
+
+//null -> standalone value intentionnaly khali chodte hai means intentionally khali hai
+
+//undefined-> value define hi n kri 
+//symbol->unique
+
+//object  
+
+console.log(typeof undefined);//output undefined
+
+console.log(typeof null);//output object
+ console.log(3+3)
