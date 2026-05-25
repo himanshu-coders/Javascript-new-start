3dc9b6d95c962f575a64dd5133eb99789f112a46
himanshu-coders <avasthihimanshu98@gmail.com>
2026-05-25 11:50:19 +0000

    todays commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=true
+// console.log( typeof score)
+// console.log(typeof (score))
+
+//javascript main strict check nahi hai isiliye log typescript use krte hai
+// "33" -> 33
+//"33abc"->Nan
+//true->1
+//false->0  
+
+let valueInNumber=Number(score)//important line for type conversion
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn=undefined
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1->true 0->false
+//""-> false " "->true
+//"hello"->true
+
+
