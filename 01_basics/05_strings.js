1c5a282c0f644e269b08025e53e5e3485533798b
himanshu-coders <avasthihimanshu98@gmail.com>
2026-05-26 11:34:28 +0000

    last commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name="himanshu"
+const repoCount=50
+//another type to declare string
+const gamename=new String("Hello duniya")
+console.log(name+"Avasthi"+repoCount) //8
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`) //Hello my name is himanshu and my repo Count is 50
+
