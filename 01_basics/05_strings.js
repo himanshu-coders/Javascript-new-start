a6d8e307e2a9d0ff585e37e875bce91152baf5ee
himanshu-coders <avasthihimanshu98@gmail.com>
2026-05-27 07:48:17 +0000

    wed commit

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,6 +2,20 @@ const name="himanshu"
 const repoCount=50
 //another type to declare string
 const gamename=new String("Hello duniya")
-console.log(name+"Avasthi"+repoCount) //8
-console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`) //Hello my name is himanshu and my repo Count is 50
+// console.log(name+"Avasthi"+repoCount) //8 
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`) //Hello my name is himanshu and my repo Count is 50
+
+console.log(gamename.length)
+// console.log(gamename.toUpperCase()) 
+// console.log(gamename.charAt(3))
+// console.log(gamename.indexOf("a"))
+// const newString=gamename.substring(0,4)
+// console.log(newString)
+const anotherString=gamename.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne="      himanshu      "
+console.log(newStringOne.trimEnd())
+const url="https://himanshu.com/hitesh%20sharma"
+console.log(url.replace('%20','-'))
 
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score=400
+// console.log(score)
+const balance=new Number(100)
+// console.log(balance)
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(3))
+const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++==maths
+
+// console.log(Math.PI)
+// console.log(Math.abs(-4)) // positive positive hi rhta hai negative positive main conveert krdeta hai 
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.floor((Math.random()*10)+1))// ye 1 se 10 ke beech ka random number dega
+
+const min=25
+const max=30
+console.log(Math.floor(Math.random()*(max-min+1))+min)//+1 isiliye taki 0 ko avoid krde
