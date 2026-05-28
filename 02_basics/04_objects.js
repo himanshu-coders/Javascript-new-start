f9b924731512359282a1561169b4b9a3592f89cc
himanshu-coders <avasthihimanshu98@gmail.com>
2026-05-28 08:15:17 +0000

    json talk

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,43 @@
+// const tinderUser=new Object()
+// const tinderUser={}
+// tinderUser.id='123abc'
+// tinderUser.name='himanshu'
+// tinderUser.isLoggedIn=false
+
+// console.log(tinderUser)
+
+// const regularUser={
+//     email:"himanshu@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"heman",
+//             lastname:"avasthi"
+//         }
+//     }
+// }
+
+
+// console.log(regularUser.fullname.userfullname)
+
+
+const obj1={
+    1:"a", 2:"b"
+}
+const obj2={
+    3:"c", 4:"d"
+}
+// const obj3= {obj1,obj2}
+// const obj3=Object.assign(obj1, obj2)
+// const obj3={...obj1,...obj2}
+// console.log(obj3)
+
+const course={
+    coursename:"javascript",
+    price:990,
+    courseInstructor:"hitesh"
+}
+// course.courseInstructor
+const {courseInstructor}=course
+// console.log(instructor)
+console.log(courseInstructor)
+
