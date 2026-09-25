f6a2e1b373fe8409fdd937ea9b4f1e002a23780e
himanshu-coders <avasthihimanshu98@gmail.com>
2026-09-25 16:00:51 +0000

    sep25commit

--- 09_advance_one/indexx.js
+++ 09_advance_one/indexx.js
@@ -8,12 +8,18 @@
  * async await vs promise.then/.catch
  */
 
-const p=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Hello")
-    },10000)
+// const p1=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Hello")
+//     },2000)
 
-})
+// })
+// const p2=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Hello")
+//     },1000)
+
+// })
 
 //older way of handling promises
 // function getData(){
@@ -23,16 +29,16 @@ const p=new Promise((resolve,reject)=>{
 // getData();
 // await is a keyword that can only be used inside an async function. It makes JavaScript wait until the promise settles and returns its result.
 //async await way
-async function handlePromise(){
-    const val= await p
-    console.log("Fetching data...")
-    console.log(val)
+// async function handlePromise(){
+//     const val= await p1
+//     console.log("Fetching data...")
+//     console.log(val)
 
-    const val2= await p
-    console.log("Fetching data...2222")
-    console.log(val2)
-}
-handlePromise()
+//     const val2= await p2
+//     console.log("Fetching data...2222")
+//     console.log(val2)
+// }
+// handlePromise()
 // async function getdata(){
 //     return p
 // }
@@ -42,3 +48,17 @@ handlePromise()
 
 //async and await are used to handle promises
 // await can be only be used inside a async function
+const API_URL="https://api.gihub.com/users/himanshu-coders"
+
+async function handlePromise(){
+
+    try{
+        const response=await fetch(API_URL)
+    const data=await response.json()
+    console.log(data)
+    }
+    catch(err){
+        console.log("Error:",err)
+    }
+}  
+handlePromise()
